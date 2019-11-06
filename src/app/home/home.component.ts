70edb39a600c2c65e6133d71b963d99a992f321b
Toni Kaplan <tonarie.k@gmail.com>
2019-11-05 21:09:13 -0500

    Add buttons to change card

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { cards } from "assets/json/statusQuestions.json";
+
+interface iCard {
+  question: string;
+  answerTitle?: string;
+  answer: string;
+  level?: 1 | 2 | 3;
+}
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  allCards: Array<iCard> = cards;
+  cardCount: number = this.allCards.length;
+  currentCardIndex: number = 0;
+  currentCard: Nullable<iCard>;
+  cardFlipped: boolean = false;
+  constructor() {}
 
   ngOnInit() {
+    this.updateCurrentCard();
+    console.log(this.allCards);
+    console.log(this.currentCard);
   }
 
+  setCardQuestionSideUp() {
+    this.cardFlipped = false;
+  }
+  setCardAnswerSideUp() {
+    this.cardFlipped = false;
+  }
+  flipCard() {
+    this.cardFlipped = !this.cardFlipped;
+  }
+
+  updateCurrentCard() {
+    this.setCardQuestionSideUp();
+    this.currentCard = this.allCards[this.currentCardIndex];
+    console.log(this.currentCard);
+  }
+
+  nextCard() {
+    this.currentCardIndex += 1;
+    this.updateCurrentCard();
+  }
+
+  previousCard() {
+    this.currentCardIndex -= 1;
+    this.updateCurrentCard();
+  }
 }
